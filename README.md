69a038939d214b9a675a2ed7ea44f9cd253ba067
Minneh Mugo <minneh.mugo@gmail.com>
2017-03-06 12:47:16 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Project Name: Pig Dice
+
+##Project Site: <https://minneh.github.io/pig_dice.github.io/>
+
+## Created by: Minneh Mugo
+
+# Description:
+Pig is a very simple dice game that is exceptionally easy both to learn and to play. As a jeopardy dice game, it manages to keep players on the edge of their seat as they try to determine whether to risk it all or to play it safe. Pig can be played virtually anywhere; the only requirements are one (or two) standard six-sided die and something to keep score with, and, of course the players. The first person to score 100 points wins!
+
+# Specifications:
+Step 1:
+Roll one die for your first turn. Playing with two dice is optional.
+
+Step 2:
+A roll of two, three, four, five, or six gains one the respective points. If you roll a one, you lose all the points you accumulated during that turn, and you lose your turn.
+
+Step 3:
+One can choose to "hold" which saves the points they have so far and shifts the turn to the other player.
+
+Tip
+Knowing when to stop rolling and end your turn is the key to winning at Pig. It's an exercise in deciding between gut instinct and greed.
+
+Step 4:
+Reach 100 points first and win the game.
+
+Step 5:
+To play with two dice, roll both for your first turn. If you roll a one on either die, you score zero for that turn and your turns ends, but if you roll two ones – or snake eyes – you lose your turn and all the points you've earned during the game.
+
+#Technologies Used:
+- HTML
+- CSS
+- JavaScript
+
+#Setup:
+- Open your preferred browser
+- Head to the url https://minneh.github.io/pig-dice.github.io/
+- Begin playing!
+
+#Contributing
+
+I welcome pull requests
+
+## History
+
+Website created on 3rd March 2017
+
+## Credits
+
+Credits to Moringa School
---
+++ js/scripts.js
@@ -0,0 +1,114 @@
+//business logic
+var player1="";
+var player2="";
+
+var throwdice = function () {
+  return Math.floor(Math.random()*6)+1;
+};
+
+function Player(turn) {
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.turn = turn;
+  this.playerName;
+};
+
+// checking for 1
+Player.prototype.rollone = function() {
+  if (this.roll === 1) {
+  this.tempscore = 0;
+  alert("Sorry " + this.playerName + ", you rolled a 1! Your turn is over!");
+  // this.changeturn();
+  } else {
+  this.tempscore += this.roll;
+  }
+};
+
+// hold
+Player.prototype.hold = function () {
+  this.totalscore += this.tempscore;
+  this.tempscore = 0;
+  alert("Your turn is over, pass the mouse!");
+};
+
+Player.prototype.checkWinner = function () {
+  if (this.totalscore >= 100) {
+    alert(this.playerName + ", you win!");
+  }
+};
+
+Player.prototype.restart = function () {
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.playerName = "";
+};
+var changePlayers = function() {
+  $(".player1Name").val("");
+  $(".player2Name").val("");
+};
+
+// User Interface
+$(document).ready(function() {
+
+  $("button#start").click(function(event){
+    player1 = new Player(true);
+    player2 =  new Player(false);
+    $("#main-window").hide();
+    $("#console").show();
+    var player1Name = $(".player1Name").val();
+    $("#player1Name").text(player1Name);
+
+    var player2Name = $(".player2Name").val();
+    $("#player2Name").text(player2Name);
+
+    player1.playerName = player1Name;
+    player2.playerName = player2Name;
+});
+  $("button#restart").click(function(event){
+    $("#console").hide();
+    changePlayers();
+    player1.restart();
+    player2.restart();
+    $("#roundTotal1").empty();
+    $("#totalScore1").empty();
+    $("#dieRoll1").empty();
+    $("#roundTotal2").empty();
+    $("#totalScore2").empty();
+    $("#dieRoll2").empty();
+
+    $("#main-window").show();
+  });
+
+  $("button#player1-roll").click(function(event){
+    player1.roll = throwdice();
+    $("#dieRoll1").text(player1.roll);
+    player1.rollone();
+    $("#roundTotal1").text(player1.tempscore);
+  });
+
+  $("button#player2-roll").click(function(event){
+    player2.roll = throwdice();
+    $("#dieRoll2").text(player2.roll);
+    player2.rollone();
+    $("#roundTotal2").text(player2.tempscore);
+  });
+
+  $("button#player1-hold").click(function(event){
+    player1.hold();
+    $("#totalScore1").text(player1.totalscore);
+    $("#roundTotal1").empty();
+    $("#dieRoll1").empty();
+    player1.checkWinner();
+  });
+
+  $("button#player2-hold").click(function(event){
+    player2.hold();
+    $("#totalScore2").text(player2.totalscore);
+    $("#roundTotal2").empty();
+    $("#dieRoll2").empty();
+    player2.checkWinner();
+  });
+
+});
