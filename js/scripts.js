e26a814a913366d337cc3e55beddf79ba26d0008
Minneh Mugo <minneh.mugo@gmail.com>
2017-03-07 08:30:34 +0300

    fix display bug

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,7 @@ Player.prototype.rollone = function() {
   if (this.roll === 1) {
   this.tempscore = 0;
   alert("Sorry " + this.playerName + ", you rolled a 1! Your turn is over!");
+
   } else {
   this.tempscore += this.roll;
   }
